784ecc7bf9235711879bd27d2894b61e19edf8a0
Jacob Smullyan <smulloni@smullyan.org>
2002-08-28 16:53:28 -0400

    a module, akin to HTMLGen or Apache's ECS (Element Construction Kit), that can be used to generate XHTML.  Parser module will come next....

---
+++ pylibs/ecs/__init__.py
@@ -0,0 +1,7 @@
+# Time-stamp: <02/08/28 16:46:42 smulloni>
+# $Id$
+
+# a library similar in intent to HTMLGen or Java's ECS, but quite minimal.
+# Should generate valid XHTML.
+
+from xhtml import *
