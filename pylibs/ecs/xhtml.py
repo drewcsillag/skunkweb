225b6b611dff51d64bc7229f3b3e3f6cf7895cd3
Jacob Smullyan <smulloni@smullyan.org>
2002-12-13 13:34:10 -0500

    added Li for those who want list items.

--- pylibs/ecs/xhtml.py
+++ pylibs/ecs/xhtml.py
@@ -1,5 +1,5 @@
-# Time-stamp: <02/11/08 15:54:01 smulloni>
-# $Id: xhtml.py,v 1.3 2002/08/29 15:37:11 smulloni Exp $
+# Time-stamp: <02/12/13 13:29:19 smulloni>
+# $Id: xhtml.py,v 1.4 2002/11/08 21:16:21 smulloni Exp $
 
 ######################################################################## 
 #  Copyright (C) 2002 Jacob Smullyan <smulloni@smullyan.org>
@@ -136,6 +136,7 @@ CONTAINER_ELEMENTS=['a',
                     'kbd',
                     'label',
                     'legend',
+                    'li',
                     'list',
                     'map',
                     'menu',
